7e23849e51f480054354b9b72a538d12ea6834ea
master <82279178@qq.com>
2021-12-19 17:55:15 +0800

    1.0

--- test/index.php
+++ test/index.php
@@ -2,4 +2,4 @@
 require_once __DIR__.'/../vendor/autoload.php';
 $test=new \Cpkj\ClientX();
 $api="https://apigx.cn/token/3ba3a7ca151511ecab09c43772bcfd94/code/twbg28/rows/1.json";
-$result=$test->getData($api,"","");
+$result=$test->getData($api);
